b6fa55e419bcb2417161903eaabceacb243e64de
aybeniz <aybenizkazimova15@gmail.com>
2024-02-10 00:16:58 +0400

    Eolymp Tasks

--- src/main/java/az/edu/turing/SumAndAmount.java
+++ src/main/java/az/edu/turing/SumAndAmount.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class SumAndAmount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int sum = 0;
         int amount = 0;
-
         while (sc.hasNext()) {
             int num = sc.nextInt();
+
 
             sum += num;
             amount++;
